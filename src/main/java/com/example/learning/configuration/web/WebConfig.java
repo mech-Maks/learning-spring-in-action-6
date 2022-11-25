105bf876e46f6fa9baefc5c223ca82a54b212b5e
mech-Maks <maksim_28_11_1999@mail.ru>
2022-11-25 13:57:56 +0300

    Basic Spring Security configuration with 2 hard-coded users

--- src/main/java/com/example/learning/configuration/web/WebConfig.java
+++ src/main/java/com/example/learning/configuration/web/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.learning.configuration;
+package com.example.learning.configuration.web;
 
 import org.springframework.context.annotation.Configuration; import
         org.springframework.web.servlet.config.annotation.ViewControllerRegistry; import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
