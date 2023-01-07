bd600891f6f50a7928fe5d70ce5dd724d1a5b8e7
mech-Maks <maksim_28_11_1999@mail.ru>
2023-01-07 23:49:05 +0300

    refactoring

--- src/main/java/com/example/learning/controllers/OrderController.java
+++ src/main/java/com/example/learning/controllers/OrderController.java
@@ -20,9 +20,11 @@ import javax.validation.Valid;
 @SessionAttributes("tacoOrder")
 public class OrderController {
     private OrderRepository orderRepo;
+
     public OrderController(OrderRepository orderRepo) {
         this.orderRepo = orderRepo;
     }
+
     @GetMapping("/current") public String orderForm() {
         return "orderForm";
     }
