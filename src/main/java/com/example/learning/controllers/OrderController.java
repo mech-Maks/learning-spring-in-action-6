787bed129128f229c7f05a4535fbee45e206fc21
mech-Maks <maksim_28_11_1999@mail.ru>
2023-01-08 20:24:18 +0300

    [p.182 - p.184] create property-value and inject it into bean + logging path

--- src/main/java/com/example/learning/controllers/OrderController.java
+++ src/main/java/com/example/learning/controllers/OrderController.java
@@ -5,8 +5,12 @@ import com.example.learning.entity.TacoOrder;
 import com.example.learning.entity.User;
 import com.example.learning.repo.OrderRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,13 +25,25 @@ import javax.validation.Valid;
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
 public class OrderController {
+    @Value("${orders.pageSize:10}")
+    private Integer pageSize;
     private OrderRepository orderRepo;
 
     public OrderController(OrderRepository orderRepo) {
         this.orderRepo = orderRepo;
     }
 
-    @GetMapping("/current") public String orderForm() {
+    @GetMapping
+    public String ordersForUser(@AuthenticationPrincipal User user, Model model) {
+        log.info("requested to fetch {} orders for user {}", pageSize, user.getFullname());
+        Pageable pageable = PageRequest.of(0, pageSize);
+        model.addAttribute("orders", orderRepo.findByUserOrderByPlacedAtDesc(user, pageable));
+
+        return "orderList";
+    }
+
+    @GetMapping("/current")
+    public String orderForm() {
         return "orderForm";
     }
 
--- src/main/java/com/example/learning/repo/OrderRepository.java
+++ src/main/java/com/example/learning/repo/OrderRepository.java
@@ -1,6 +1,8 @@
 package com.example.learning.repo;
 
 import com.example.learning.entity.TacoOrder;
+import com.example.learning.entity.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
@@ -9,4 +11,6 @@ import java.util.List;
 public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
     @Query("select o from TacoOrder o where o.deliveryCity='Seattle'")
     List<TacoOrder> readOrdersDeliveredInSeattle();
+
+    List<TacoOrder> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
 }
